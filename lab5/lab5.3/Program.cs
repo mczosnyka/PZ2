12b1f0d30fd96fbc532c4edcf4473d9d1b1fe4bb
mczosnyka <115806957+mczosnyka@users.noreply.github.com>
2024-04-17 16:02:37 +0200

    Add files via upload

---
+++ lab5/lab5.3/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualBasic;
+
+public class Program{
+    static string matchingfile;
+    static void Main(string[] args)  
+    {
+        Console.WriteLine("podaj nazwe do wyszukania");
+        string matcher = Console.ReadLine();
+        Console.WriteLine("podaj sciezke do katalogu bazowego");
+        string path = Console.ReadLine();
+        Thread finder = new Thread(() => Finder(matcher, path));
+        finder.Start();
+        while(matchingfile is null){
+            Thread.Sleep(100);
+        }
+        Console.WriteLine("znaleziono plik: " + matchingfile);
+    }
+
+    static void Finder(string match, string path){
+        string[] files = Directory.GetFiles(path);
+        foreach (string file in files)
+        {
+            if(file.Contains(match)){
+                matchingfile = file;
+                break;
+            }
+        }
+        if(matchingfile is null){
+            string[] dirs = Directory.GetDirectories(path);
+            foreach(string dir in dirs){
+                Thread finder = new Thread(() => Finder(match, dir));
+                finder.Start();
+            }
+        }
+    }
+    }
---
+++ lab5/lab5.4/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualBasic;
+
+public class Program{
+    static bool threadRunning = true;
+    static Dictionary<Thread, bool> threads = new Dictionary<Thread, bool>();
+    static void Main(string[] args)  
+    {
+        Console.WriteLine("podaj liczbe watkow");
+        int n = int.Parse(Console.ReadLine());
+        for(int j = 0; j<n; j++){
+            Thread th = new Thread(() => Threader());
+            threads.Add(th, false);
+            th.Start();
+        }
+        if(threads.Count==n){
+            int z = 0;
+            foreach(var thr in threads){
+                if(thr.Value == false){
+                    break;
+                }
+                z ++;
+            }
+            if(z == n){
+                Console.WriteLine("All threads are running");
+            }
+            z = 0;
+            threadRunning = false;
+            while(true){
+                foreach(var thr in threads){
+                if(thr.Value == true){
+                    break;
+                }
+                z++;
+            }
+            if(z == n){
+                Console.WriteLine("No threads are running");
+                break;
+            }
+            }
+
+        }
+    }
+
+    static void Threader(){
+        threads[Thread.CurrentThread] = true;
+        while (threadRunning)
+        {
+            Thread.Sleep(1000); 
+        }
+        threads[Thread.CurrentThread] = false;
+    }
+    }
